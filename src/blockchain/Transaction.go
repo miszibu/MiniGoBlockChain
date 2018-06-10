31982891138d157d20d0044411894a286a5a87f0
miszibu <806768001@qq.com>
2018-06-11 00:11:18 +0800

    unspentpool

--- src/blockchain/Transaction.go
+++ src/blockchain/Transaction.go
@@ -5,9 +5,6 @@ type Transaction struct {
 	Sender string		// 发送方地址
 	Recipient string    // 接收方地址
 	Amount float64		// 交易金额
-	IsValid bool		// 是否有效
-	// 分布式数据库 怎么修改isvalid
-	// 加入unspendpool概念
 }
 
 func (tx *Transaction)GetId() string {
